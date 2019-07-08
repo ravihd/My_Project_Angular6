36894e349df634e9517d0ab71f1385c05ab441be
Your Name <ravihd54@gmail.com>
2019-07-08 11:12:55 +0530

    Updtaed login Component

--- src/app/login-comp/login-comp.component.ts
+++ src/app/login-comp/login-comp.component.ts
@@ -10,40 +10,41 @@ import { MyserviceService } from '../myservice.service';
 export class LoginCompComponent implements OnInit {
 
   Userdetails: any;
+  status;
 
   constructor(private router: Router, private service: MyserviceService) { }
 
   ngOnInit() {
-
     this.service.getlogindetails().subscribe(data => this.Updateuserdetails(data))
-
   }
 
-  gotoHome(data) {
-    ``
-    console.log("User name :  ", data.emailid, "password : ", data.password);
-    console.log("Here>", this.Userdetails)
-
-    this.Userdetails.forEach(value => {
-
-      if (data.emailid == value.email) {
-
-        if (data.password == value.Password) {
-
-          this.service.setValue(value.FullName)
-
-          this.router.navigateByUrl("logout")
-        }else{
-          alert("Invalid password")
-          
-        }
-      } 
-    })
+  gotoHome(data:any) {
+   this.Check_Credentials(data) 
+   console.log("in go home", this.status) 
+   if (this.status==true) {
+    console.log("in side if ",this.status)
+     this.router.navigateByUrl("logout")
+     
+   } else {
+     alert("Invalid Credestials")
+   }
   }
 
   Updateuserdetails(data: any) {
     this.Userdetails = data
   }
 
+  Check_Credentials(data: any){
+    this.Userdetails.forEach(value => {
+
+      if (data.emailid == value.email && data.password == value.Password) {
+          this.service.setValue(value.FullName)
+          this.status=true;
+          console.log(this.status)
+          return ;
+             
+      } 
+    })
+  }
 
 }
